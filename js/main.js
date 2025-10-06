ec95974f450dda0087d9bbf7140d009bca3e6db0
huylam0703 <huylamnhat244@gmail.com>
2025-10-07 01:26:33 +0700

    code search mentor and profile mentor

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+  // Tabs chuyển đổi Hồ sơ / Đánh giá
+  const tabs = document.querySelectorAll('.mentor-tab');
+  const contents = document.querySelectorAll('.tab-content');
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.classList.remove('active'));
+      contents.forEach(c => c.classList.remove('active'));
+      tab.classList.add('active');
+      document.getElementById(tab.dataset.tab).classList.add('active');
+    });
+  });
+
